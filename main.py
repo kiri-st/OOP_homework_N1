0992ba4b9cabc34a3a11b14ed5a571c12af11dfe
kiri_st <kiri_s.20-91x2@mail.ru>
2023-11-29 15:42:16 +0500

    Task completed

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 class Student:
+
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
         self.gender = gender
         self.finished_courses = []
         self.courses_in_progress = []
-    grades = {}
-    def rate_lectur(self, lectur, course, grades_lectur):
+        self.grades = {}
+        self.average_grade = 0
+
+    def rate_lectur(self, lectur, course, grades):
         if (isinstance(lectur, Lecturer) and course in lectur.courses_attached and course in self.courses_in_progress
-        and grades_lectur in range(0,11)):
-            if course in lectur.grades_lectur:
-                lectur.grades_lectur[course] += [grades_lectur]
+        and grades in range(0,11)):
+            if course in lectur.grades:
+                lectur.grades[course] += [grades]
             else:
-                lectur.grades_lectur[course] = [grades_lectur]
+                lectur.grades[course] = [grades]
+            lectur.average_grade = lectur.get_average_grade()
         else:
             return 'Ошибка'
-    def average_grade_student(self):
+    def get_average_grade(self):
         return sum(sum(self.grades.values(), [])) / len(sum(self.grades.values(), []))
     def __str__(self):
         return (f'Имя: {self.name}\n'
                 f'Фамилия: {self.surname}\n'
-                f'Средняя оценка за домашние задания: {self.average_grade_student()}\n'
+                f'Средняя оценка за домашние задания: {self.average_grade}\n'
                 f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
                 f'Завершенные курсы: {", ".join(self.finished_courses)}')
     def __lt__(self, other):
-        return self.average_grade_student() < other.average_grade_lectur()
+        return self.average_grade < other.average_grade
     def __gt__(self, other):
-        return self.average_grade_student() > other.average_grade_lectur()
+        return self.average_grade > other.average_grade
     def __eq__(self, other):
-        return self.average_grade_student() == other.average_grade_lectur()
+        return self.average_grade == other.average_grade
     def __ne__(self, other):
-        return self.average_grade_student() != other.average_grade_lectur()
+        return self.average_grade != other.average_grade
 # ======================================================================================================================
 class Mentor:
     def __init__(self, name, surname):
@@ -41,21 +45,22 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-    grades_lectur = {}
-    def average_grade_lectur(self):
-        return sum(sum(self.grades_lectur.values(), [])) / len(sum(self.grades_lectur.values(), []))
+        self.grades = {}
+        self.average_grade = 0
+    def get_average_grade(self):
+        return sum(sum(self.grades.values(), [])) / len(sum(self.grades.values(), []))
     def __str__(self):
         return (f'Имя: {self.name}\n'
                 f'Фамилия: {self.surname}\n'
-                f'Средняя оценка за лекции: {self.average_grade_lectur()}')
+                f'Средняя оценка за лекции: {self.average_grade}')
     def __lt__(self, other):
-        return self.average_grade_lectur() < other.average_grade_student()
+        return self.average_grade < other.average_grade
     def __gt__(self, other):
-        return self.average_grade_lectur() > other.average_grade_student()
+        return self.average_grade > other.average_grade
     def __eq__(self, other):
-        return self.average_grade_lectur() == other.average_grade_student()
+        return self.average_grade == other.average_grade
     def __ne__(self, other):
-        return self.average_grade_lectur() != other.average_grade_student()
+        return self.average_grade != other.average_grade
 # ----------------------------------------------------------------------------------------------------------------------
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -67,6 +72,7 @@ class Reviewer(Mentor):
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
+            student.average_grade = student.get_average_grade()
         else:
             return 'Ошибка'
     def __str__(self):
@@ -152,13 +158,28 @@ print(lecturer_2 != student_2)
 # Подсчёт средней оценки за домашние задания по всем студентам в рамках курса:
 students = [student_1, student_2, student_3]
 def overall_rating_students(students, course):
-    return sum(Student.grades.get(course)) / len(students)
-print(round(overall_rating_students(students, 'Python')))
-print(round(overall_rating_students(students, 'Git')))
+    grades = 0
+    for student in students:
+        grade_course = student.grades.get(course)
+        grades += sum(grade_course) / len(grade_course)
+    return grades / len(students)
+print()
+print(f"Средняя оценка за домашние задания по всем студентам в рамках курса Python: "
+      f"{round(overall_rating_students(students, 'Python'), 2)}")
+print(f"Средняя оценка за домашние задания по всем студентам в рамках курса Git: "
+      f"{round(overall_rating_students(students, 'Git'), 2)}")
 
+# ----------------------------------------------------------------------------------------------------------------------
 # Подсчёт средней оценки за лекции всех лекторов в рамках курса:
 lectors = [lecturer_1, lecturer_2]
 def overall_rating_lectors(lectors, course):
-    return sum(Lecturer.grades_lectur.get(course)) / len(lectors)
-print(round(overall_rating_lectors(lectors, 'Python')))
-print(round(overall_rating_lectors(lectors, 'Git')))
+    grades = 0
+    for lector in lectors:
+        grade_course = lector.grades.get(course)
+        if grade_course is not None:
+            grades += sum(grade_course) / len(grade_course)
+    return grades
+print(f"Средняя оценка за лекции всех лекторов в рамках курса Python: "
+      f"{round(overall_rating_lectors(lectors, 'Python'), 2)}")
+print(f"Средняя оценка за лекции всех лекторов в рамках курса Git: "
+      f"{round(overall_rating_lectors(lectors, 'Git'), 2)}")
